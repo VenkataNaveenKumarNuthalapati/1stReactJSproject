9d5c64a6d3a41fa19cc1c12c3c4cee1bd87bc5c1
VenkataNaveenKumarNuthalapati <venkatanaveenkumarnuthalapati@gmail.com>
2023-02-23 21:18:27 +0000

    my1stReactJSproject

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <h1 className="mainHeading">Super Over League</h1>
+    <div>
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      >
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+      >
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
